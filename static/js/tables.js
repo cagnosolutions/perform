0a00aee2b26863f560fb505714a83a9a563deb75
therealscout <calebcagno@gmail.com>
2016-12-04 14:41:59 -0500

    updated dataTables

--- static/js/tables.js
+++ static/js/tables.js
@@ -1,32 +1,3 @@
 $(document).ready(function() {
-
-    $('#example').dataTable();
-
-    $('#example2').dataTable();
-
-    $('#example3').dataTable();
-
-    $('#example4').dataTable();
-
-    $('#example5').dataTable();
-
-    $('#example6').dataTable();
-
-    $('#example7').dataTable();
-
-    $('#example8').dataTable();
-
-    $('#example9').dataTable();
-
-    $('#example10').dataTable();
-
-    $('#example11').dataTable();
-
-    $('#example12').dataTable();
-
-    $('#example13').dataTable();
-
-    $('#example14').dataTable();
-
-    $('#example15').dataTable();
+    $('.dataTable').dataTable();
 });
